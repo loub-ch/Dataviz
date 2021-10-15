7b9f96ecf2e9c334b5e41f24aa1294319d3d90df
Loubna Chahboune <84322854+loub-ch@users.noreply.github.com>
2021-10-15 14:36:12 +0200

    Add files via upload

---
+++ repartition.R
@@ -0,0 +1,374 @@
+source("mef_filtrer_sta_annee.R")
+library(plotly)
+
+
+repartition<-function(df,pa,da){
+  dt<-mef_filtrer_sta_annee(df, premiere_annee =pa,derniere_annee =da)
+  dt<-sqldf('select distinct(pop_id),sta_id,avg(ipr) as ipr from dt group by pop_id')
+  ##Rajout des classes de qualitees
+  dt<-sqldf('SELECT *,case when ipr <"7" then "Tres bon"
+                 when ipr  >="7" and ipr <"16" then "Bon"
+                when ipr >="16" and  ipr < "25" then "Moyen"
+                when ipr >="25" and ipr<"36" then "Mediocre"
+                else "Mauvais"
+                end as classe_ipr FROM dt')
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+
+  #Repartition de la classe de qualitee
+  #calcul de nombre de stations selon les classes de qualitees
+  dt<-sqldf('SELECT classe_ipr,count( classe_ipr) as nbre_class  FROM
+                  dt group by classe_ipr ')
+
+  #Creation de %
+  dt<- dt %>%
+    mutate(nbre_class_pr =round(prop.table(nbre_class)*100))
+
+
+
+  dt<-dt %>%mutate(lab.ypos = cumsum(nbre_class_pr) - 0.5*nbre_class_pr)
+  dt<-sqldf('SELECT *  FROM dt order by  CASE classe_ipr
+WHEN "Tres bon" THEN 1
+WHEN "Bon" THEN 2
+WHEN "Moyen" THEN 3
+WHEN "Mediocre" THEN 4
+ELSE 5 end ')
+
+  dt<-dt%>%mutate(
+    type= case_when(
+      classe_ipr=="Tres bon" ~ "Tr\U00E8s bon",
+      classe_ipr=="Bon" ~ "Bon",
+      classe_ipr=="Moyen" ~ "Moyen",
+      classe_ipr=="Mediocre" ~"M\U00E9\U0064iocre",
+      classe_ipr=="Mauvais" ~ "Mauvais"
+    )
+  )
+
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+  fig <- dt %>%
+    plot_ly(labels = ~type, values = ~nbre_class_pr,texttemplate="%{percent:.0%f}",marker = list(colors =~color),direction = "clockwise",
+            hoverinfo="text",hovertext= ~paste(nbre_class,"stations"))%>%
+    add_pie(hole = 0.6,sort=F)%>%
+    layout(showlegend =T,
+                        xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                        yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+    layout(legend = list(x = 100, y = 0.5),paper_bgcolor = "transparent")
+  fig
+
+}
+repartition(data_net,2019,2020)
+repartition_dep<-function(df,pa,da){
+  dt<-mef_filtrer_sta_annee(df, premiere_annee =pa,derniere_annee =da)
+  dt<-sqldf('select distinct(dep_libelle),avg(ipr) as ipr from dt group by dep_libelle')
+  ##Rajout des classes de qualitees
+  dt<-sqldf('SELECT *,case when ipr <="7" then "Tres bon"
+                 when ipr  >"5" and ipr <= "16" then "Bon"
+                when ipr >"16" and  ipr <= "25" then "Moyen"
+                when ipr >"25" and ipr<= "36" then "Mediocre"
+                else "Mauvais"
+                end as classe_ipr FROM dt')
+
+  #Repartition de la classe de qualitee
+  #calcul de nombre de stations selon les classes de qualitees
+  dt<-sqldf('SELECT classe_ipr,count( classe_ipr) as nbre_class  FROM
+                  dt group by classe_ipr ')
+
+  #Creation de %
+  dt<- dt %>%
+    
+    mutate(nbre_class_pr =round(prop.table(nbre_class)*100))
+
+
+
+  dt<-dt %>%
+  
+    mutate(lab.ypos = cumsum(nbre_class_pr) - 0.5*nbre_class_pr)
+  dt<-sqldf('SELECT *  FROM dt order by  CASE classe_ipr
+WHEN "Tres bon" THEN 1
+WHEN "Bon" THEN 2
+WHEN "Moyen" THEN 3
+WHEN "Mediocre" THEN 4
+ELSE 5 end ')
+
+  dt<-dt%>%mutate(
+    type= case_when(
+      classe_ipr=="Tres bon" ~ "Tr\U00E8s bon",
+      classe_ipr=="Bon" ~ "Bon",
+      classe_ipr=="Moyen" ~ "Moyen",
+      classe_ipr=="Mediocre" ~"M\U00E9\U0064iocre",
+      classe_ipr=="Mauvais" ~ "Mauvais"
+    )
+  )
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+  fig <- dt %>% 
+    plot_ly(labels = ~classe_ipr, values = ~nbre_class_pr,marker = list(colors =~color),direction = "clockwise",
+            hoverinfo="text",hovertext= ~paste(nbre_class,"departements"))%>% 
+    add_pie(hole = 0.6,sort=F)%>% 
+    layout(showlegend =T,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+    # labs(caption =paste("valeur de l'IPR sur",b,"départements",":\n", 
+    #                                            "Tres bon (",d,")"," Bon (",e,")","Moyen (",f,")","Mediocre (",g,")","Mauvais (",h,")"))
+    #                      
+    layout(legend = list(x = 100, y = 0.5),paper_bgcolor = "transparent")
+  #animation_opts_defaults()
+  fig
+  }
+
+
+repartition_reg<-function(df,pa,da){
+  dt<-mef_filtrer_sta_annee(df, premiere_annee =pa,derniere_annee =da)
+  dt<-sqldf('select distinct(reg_code_insee),avg(ipr) as ipr from dt group by reg_code_insee ')
+  ##Rajout des classes de qualitees
+  dt<-sqldf('SELECT *,case when ipr <="7" then "Tres bon"
+                 when ipr  >"5" and ipr <= "16" then "Bon"
+                when ipr >"16" and  ipr <= "25" then "Moyen"
+                when ipr >"25" and ipr<= "36" then "Mediocre"
+                else "Mauvais"
+                end as classe_ipr FROM dt')
+  #Repartition de la classe de qualitee
+  #calcul de nombre de stations selon les classes de qualitees
+  dt<-sqldf('SELECT classe_ipr,count( classe_ipr) as nbre_class  FROM
+                  dt group by classe_ipr ')
+
+  #Creation de %
+  dt<- dt %>%
+  
+    mutate(nbre_class_pr = round(prop.table(nbre_class)*100))
+
+
+
+  dt<-dt %>%
+    
+    mutate(lab.ypos = cumsum(nbre_class_pr) - 0.5*nbre_class_pr)
+  
+  dt<-dt%>%mutate(
+    type= case_when(
+      classe_ipr=="Tres bon" ~ "Tr\U00E8s bon",
+      classe_ipr=="Bon" ~ "Bon",
+      classe_ipr=="Moyen" ~ "Moyen",
+      classe_ipr=="Mediocre" ~"M\U00E9\U0064iocre",
+      classe_ipr=="Mauvais" ~ "Mauvais"
+    )
+  )
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+  fig <- dt %>% 
+    plot_ly(labels = ~type, values = ~nbre_class_pr,marker=list(colors =~color),direction = "clockwise",
+            hoverinfo="text",hovertext= ~paste(nbre_class,"regions"))%>% 
+    add_pie(hole = 0.6,sort=F,line= list(colors =dt$color))%>% 
+    layout(showlegend =T,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+    # labs(caption =paste("valeur de l'IPR sur",b,"départements",":\n", 
+    #                                            "Tres bon (",d,")"," Bon (",e,")","Moyen (",f,")","Mediocre (",g,")","Mauvais (",h,")"))
+    #                      
+    layout(legend = list(x = 100, y = 0.5),paper_bgcolor = "transparent")
+  #animation_opts_defaults()
+  fig
+}
+
+repartition_cd<-function(df,pa,da){
+  dt<-mef_filtrer_sta_annee(df, premiere_annee =pa,derniere_annee =da)
+  dt<-sqldf('select distinct(enh_code_sandre),avg(ipr) as ipr from dt group by enh_code_sandre ')
+  ##Rajout des classes de qualitees
+  dt<-sqldf('SELECT *,case when ipr <="7" then "Tres bon"
+                 when ipr  >"5" and ipr <= "16" then "Bon"
+                when ipr >"16" and  ipr <= "25" then "Moyen"
+                when ipr >"25" and ipr<= "36" then "Mediocre"
+                else "Mauvais"
+                end as classe_ipr FROM dt')
+  #Repartition de la classe de qualitee
+  #calcul de nombre de stations selon les classes de qualitees
+  dt<-sqldf('SELECT classe_ipr,count( classe_ipr) as nbre_class  FROM
+                  dt group by classe_ipr ')
+  
+  #Creation de %
+  dt<- dt %>%
+    
+    mutate(nbre_class_pr =round(prop.table(nbre_class)*100))
+  
+  
+  
+  dt<-dt %>%
+    
+    mutate(lab.ypos = cumsum(nbre_class_pr) - 0.5*nbre_class_pr)
+  dt<-sqldf('SELECT *  FROM dt order by  CASE classe_ipr
+WHEN "Tres bon" THEN 1
+WHEN "Bon" THEN 2
+WHEN "Moyen" THEN 3
+WHEN "Mediocre" THEN 4
+ELSE 5 end ')
+  
+  dt<-dt%>%mutate(
+    type= case_when(
+      classe_ipr=="Tres bon" ~ "Tr\U00E8s bon",
+      classe_ipr=="Bon" ~ "Bon",
+      classe_ipr=="Moyen" ~ "Moyen",
+      classe_ipr=="Mediocre" ~"M\U00E9\U0064iocre",
+      classe_ipr=="Mauvais" ~ "Mauvais"
+    )
+  )
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+  fig <- dt %>% 
+    plot_ly(labels = ~type, values = ~nbre_class_pr,texttemplate="%{percent:.0%f}",marker = list(colors =~color),direction = "clockwise",
+            hoverinfo="text",hovertext= ~paste(nbre_class,"cours d eaus"))%>% 
+    add_pie(hole = 0.6,sort=F)%>% 
+    layout(showlegend =T,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+    # labs(caption =paste("valeur de l'IPR sur",b,"cours d eau",":\n", 
+    #                                            "Tres bon (",d,")"," Bon (",e,")","Moyen (",f,")","Mediocre (",g,")","Mauvais (",h,")"))
+    #                      
+    layout(legend = list(x = 100, y = 0.5),paper_bgcolor = "transparent")
+  #animation_opts_defaults()
+  fig
+}
+
+repartition_bv<-function(df,pa,da){
+  dt<-mef_filtrer_sta_annee(df, premiere_annee =pa,derniere_annee =da)
+  dt<-sqldf('select distinct(BV),avg(ipr) as ipr from dt group by BV')
+  ##Rajout des classes de qualitees
+  dt<-sqldf('SELECT *,case when ipr <="7" then "Tres bon"
+                 when ipr  >"5" and ipr <= "16" then "Bon"
+                when ipr >"16" and  ipr <= "25" then "Moyen"
+                when ipr >"25" and ipr<= "36" then "Mediocre"
+                else "Mauvais"
+                end as classe_ipr FROM dt')
+  #Repartition de la classe de qualitee
+  #calcul de nombre de stations selon les classes de qualitees
+  dt<-sqldf('SELECT classe_ipr,count( classe_ipr) as nbre_class  FROM
+                  dt group by classe_ipr ')
+  
+  #Creation de %
+  dt<- dt %>%
+    
+    mutate(nbre_class_pr =round(prop.table(nbre_class)*100))
+  
+  
+  
+  dt<-dt %>%
+    
+    mutate(lab.ypos = cumsum(nbre_class_pr) - 0.5*nbre_class_pr)
+  dt<-sqldf('SELECT *  FROM dt order by  CASE classe_ipr
+WHEN "Tres bon" THEN 1
+WHEN "Bon" THEN 2
+WHEN "Moyen" THEN 3
+WHEN "Mediocre" THEN 4
+ELSE 5 end ')
+  
+
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+  dt<-dt%>%mutate(
+    type= case_when(
+      classe_ipr=="Tres bon" ~ "Tr\U00E8s bon",
+      classe_ipr=="Bon" ~ "Bon",
+      classe_ipr=="Moyen" ~ "Moyen",
+      classe_ipr=="Mediocre" ~"M\U00E9\U0064iocre",
+      classe_ipr=="Mauvais" ~ "Mauvais"
+    )
+  )
+  fig <- dt %>% 
+    plot_ly(labels = ~type, values = ~nbre_class_pr,marker = list(colors =~color),direction = "clockwise",
+            hoverinfo="text",hovertext= ~paste(nbre_class,"bassins hydrographiques"))%>% 
+    add_pie(hole = 0.6,sort=F)%>% 
+    layout(showlegend =T,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+    # labs(caption =paste("valeur de l'IPR sur",b,"bassins hydrographiques",":\n", 
+    #                                            "Tres bon (",d,")"," Bon (",e,")","Moyen (",f,")","Mediocre (",g,")","Mauvais (",h,")"))
+    #                      
+    layout(legend = list(x = 100, y = 0.5),paper_bgcolor = "transparent")
+  #animation_opts_defaults()
+  fig
+}
+
+repartition_ssbv<-function(df,pa,da){
+  dt<-mef_filtrer_sta_annee(df, premiere_annee =pa,derniere_annee =da)
+  dt<-sqldf('select distinct(NomSsBassi),avg(ipr) as ipr from dt group by NomSsBassi')
+  ##Rajout des classes de qualitees
+  dt<-sqldf('SELECT *,case when ipr <="7" then "Tres bon"
+                 when ipr  >"5" and ipr <= "16" then "Bon"
+                when ipr >"16" and  ipr <= "25" then "Moyen"
+                when ipr >"25" and ipr<= "36" then "Mediocre"
+                else "Mauvais"
+                end as classe_ipr FROM dt')
+  #Repartition de la classe de qualitee
+  #calcul de nombre de stations selon les classes de qualitees
+  dt<-sqldf('SELECT classe_ipr,count( classe_ipr) as nbre_class  FROM
+                  dt group by classe_ipr ')
+  
+  #Creation de %
+  dt<- dt %>%
+    
+    mutate(nbre_class_pr =round(prop.table(nbre_class)*100))
+  
+  
+  
+  dt<-dt %>%
+    
+    mutate(lab.ypos = cumsum(nbre_class_pr) - 0.5*nbre_class_pr)
+  dt<-sqldf('SELECT *  FROM dt order by  CASE classe_ipr
+WHEN "Tres bon" THEN 1
+WHEN "Bon" THEN 2
+WHEN "Moyen" THEN 3
+WHEN "Mediocre" THEN 4
+ELSE 5 end ')
+  
+  dt<-dt%>%mutate(
+    type= case_when(
+      classe_ipr=="Tres bon" ~ "Tr\U00E8s bon",
+      classe_ipr=="Bon" ~ "Bon",
+      classe_ipr=="Moyen" ~ "Moyen",
+      classe_ipr=="Mediocre" ~"M\U00E9\U0064iocre",
+      classe_ipr=="Mauvais" ~ "Mauvais"
+    )
+  )
+  dt<-sqldf('SELECT *,case when classe_ipr=="Tres bon" then "blue"
+                 when classe_ipr=="Bon" then "green"
+                when classe_ipr=="Moyen" then "yellow"
+                when classe_ipr=="Mediocre" then "orange"
+                else "red"
+                end as color FROM dt')
+  fig <- dt %>% 
+    plot_ly(labels = ~type, values = ~nbre_class_pr,marker = list(colors =~color),direction = "clockwise",
+            hoverinfo="text",hovertext= ~paste(nbre_class,"sous bassins hydrographiques"))%>% 
+    add_pie(hole = 0.6,sort=F)%>% 
+    layout(showlegend =T,
+           xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+           yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))%>%
+    # labs(caption =paste("valeur de l'IPR sur",b,"sous bassins hydrographiques",":\n", 
+    #                                            "Tres bon (",d,")"," Bon (",e,")","Moyen (",f,")","Mediocre (",g,")","Mauvais (",h,")"))
+    #                      
+    layout(legend = list(x = 100, y = 0.5),paper_bgcolor = "transparent")
+  #animation_opts_defaults()
+  fig
+}
